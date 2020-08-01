cdbf5810214ade165409f724bc89a247816dc328
ashoksai7 <67853073+ashoksai7@users.noreply.github.com>
2020-08-01 13:07:07 +0530

    Add files via upload
    Fixed few errors in the codes

--- CORS_API_Sample.js
+++ CORS_API_Sample.js
@@ -1,6 +1,8 @@
 var request = new XMLHttpRequest()
 
-var url_string = 'https://api.domainsdb.info/v1/domains/search?domain=facebook&zonecom'; // Anyone with the Tokens following '?' can access the data
+// Need a cors-anywhere proxy to access the data as the browser would block the request
+
+var url_string = 'https://cors-anywhere.herokuapp.com/https://api.domainsdb.info/v1/domains/search?domain=facebook&zonecom'; 
 
 // Open a new connection, using the GET request on the URL endpoint
 
@@ -13,10 +15,10 @@ request.onload = function() {
   // Begin accessing JSON data here
 
 var data = JSON.parse(this.response)
-
-}
-
-
 console.log(data);
 
 }
+
+
+
+
